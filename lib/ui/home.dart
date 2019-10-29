bd152b16a785b7b83e44699b983279eb3ae99467
Howard Tseng <howardt12345@users.noreply.github.com>
2019-10-29 14:53:19 -0400

    Testing animated tab bar

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,7 +1,32 @@
 import "package:flutter/material.dart";
 import 'package:flutter_web_test/utils/functions.dart';
 
+import 'animated_tab_bar/animated_tab_bar.dart';
+
 class Home extends StatefulWidget {
+  final List<BarItem> barItems = [
+    BarItem(
+      text: "Home",
+      iconData: Icons.home,
+      color: Colors.indigo,
+    ),
+    BarItem(
+      text: "Portfolio",
+      iconData: Icons.camera_alt,
+      color: Colors.pinkAccent,
+    ),
+    BarItem(
+      text: "Projects",
+      iconData: Icons.folder_open,
+      color: Colors.yellow.shade900,
+    ),
+    BarItem(
+      text: "About",
+      iconData: Icons.person_outline,
+      color: Colors.teal,
+    ),
+  ];
+
   @override
   _HomeState createState() => _HomeState();
 }
@@ -10,7 +35,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
   double _screenSize = 0;
-
+  int selectedBarIndex = 0;
 
   @override
   void initState() {
@@ -28,18 +53,47 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   Widget build(BuildContext context) {
     setState(() => _screenSize = screenWidth(context: context));
 
-
     return OrientationBuilder(
-      builder: (context, orientation) => Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text("Test"),
-          ),
+      builder: (context, orientation) =>
+          (orientation == Orientation.portrait || _screenSize <= 600)
+              ? _buildVerticalLayout()
+              : _buildHorizontalLayout(),
+    );
+  }
+
+  _buildVerticalLayout() {
+    return Scaffold(
+      appBar: PreferredSize(
+        child: AnimatedTabBar(
+          barItems: widget.barItems,
+          animationDuration: const Duration(milliseconds: 150),
+          barStyle: BarStyle(fontSize: 20.0, iconSize: 30.0),
+          onBarTap: (index) {
+            setState(() {
+              selectedBarIndex = index;
+            });
+          },
         ),
-        body: Container(
-          child: Center(
-            child: Text("Hello, web!"),
-          ),
+        preferredSize: new Size.fromHeight(56.0)
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello, web! $selectedBarIndex"),
+        ),
+      ),
+    );
+  }
+
+  _buildHorizontalLayout() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Test"),
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello, web!"),
         ),
       ),
     );
