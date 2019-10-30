5ae51c75f8f80a9621be61f92abdcc33aee255e0
Howard Tseng <howardt12345@users.noreply.github.com>
2019-10-30 00:27:39 -0400

    Working on home page

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_test/utils/custom_icons.dart';
+import 'package:flutter_web_test/utils/functions.dart';
+import 'dart:html' as html;
 
 class HomePage extends StatefulWidget {
   @override
@@ -24,7 +27,57 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Home"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RichText(
+              text: TextSpan(
+                text: "Hi, I'm Howard!",
+                style: Theme.of(context).textTheme.title.copyWith(
+                    fontSize: 72
+                ),
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                text: "An Engineer, Flutter Developer, and Photographer",
+                style: Theme.of(context).textTheme.title.copyWith(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w300
+                ),
+              ),
+            ),
+            ButtonBar(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                      CustomIcons.facebook_squared
+                  ),
+                  onPressed: () => html.window.open("https://www.facebook.com/howardt12345", "Facebook"),
+                ),
+                IconButton(
+                  icon: Icon(
+                      CustomIcons.instagram
+                  ),
+                  onPressed: () => html.window.open("https://www.instagram.com/howardt12345", "Facebook"),
+                ),
+                IconButton(
+                  icon: Icon(
+                      CustomIcons.github_circled
+                  ),
+                  onPressed: () => html.window.open("https://www.github.com/howardt12345", "Facebook"),
+                ),
+                IconButton(
+                  icon: Icon(
+                      CustomIcons.linkedin_squared
+                  ),
+                  onPressed: () => html.window.open("https://www.linkedin.com/in/howardt12345", "Facebook"),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
